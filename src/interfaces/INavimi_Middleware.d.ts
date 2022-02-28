2a920d743ae6e0ae350dbcfa9926bca3d4c24538
Ivan Valadares <ivanvaladares@hotmail.com>
2022-02-27 21:19:14 -0500

    Fix interface

--- src/interfaces/INavimi_Middleware.d.ts
+++ src/interfaces/INavimi_Middleware.d.ts
@@ -1,4 +1,4 @@
 interface INavimi_Middlewares {
     addMiddlewares: (middlewares: INavimi_Middleware[]) => void;
-    executeMiddlewares: (abortController: AbortController, context: INavimi_Context, callback: (url: string, params: INavimi_KeyList<any>) => void) => Promise<void>;
+    executeMiddlewares: (abortController: AbortController, context: INavimi_Context, callback: (url: string, params: INavimi_KeyList<any>) => void) => Promise<any>;
 }
