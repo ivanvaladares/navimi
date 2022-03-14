f1010d908d41bb22ef3b25f522d788642a28bcfb
Ivan Valadares <ivanvaladares@hotmail.com>
2022-03-13 22:01:06 -0400

    Add tests

--- tests/unit-tests/components.test.js
+++ tests/unit-tests/components.test.js
@@ -113,7 +113,7 @@ describe('Test components -', () => {
     });
 
 
-    it('Test event', (done) => {
+    it('Test event handling', (done) => {
 
         navimiComponents.registerComponent('click-component', class {
 
@@ -156,7 +156,7 @@ describe('Test components -', () => {
 
     });
 
-    it('Test event 2', (done) => {
+    it('Test event handling 2', (done) => {
 
         const component = dom.window.document.querySelector("click-component");
         
@@ -171,5 +171,68 @@ describe('Test components -', () => {
         }, 10);
 
     });
+
+    it('Test shouldUpdate 1', (done) => {
+
+        navimiComponents.registerComponent('counter-component', class {
+
+            shouldUpdate(prevAttributes, nextAttributes) {
+                return prevAttributes.count !== nextAttributes.count;
+            }
+
+            render() {
+                return `<div id="div-count">${this.props.attributes.count}</div>
+                        <div id="div-date">${new Date().getTime()}</div>`;
+            }
+
+        });
+
+        dom.window.document.querySelector("body").insertAdjacentHTML('beforeend', `
+            <counter-component count=1></counter-component>
+        `);
+        
+        setTimeout(() => {
+
+            const component = dom.window.document.querySelector("counter-component");
+
+            const counter1 = component.querySelector("#div-count").innerHTML;
+            const timer1 = component.querySelector("#div-date").innerHTML;
+
+            expect(component.props.attributes.count).to.be.equal('1');
+            expect(component.props.attributes.count).to.be.equal(counter1);
+
+            component.setAttribute("count", '2');
+
+            setTimeout(() => {
+
+                const counter2 = component.querySelector("#div-count").innerHTML;
+                const timer2 = component.querySelector("#div-date").innerHTML;
+
+                expect(component.props.attributes.count).to.be.equal('2');
+                expect(component.props.attributes.count).to.be.equal(counter2);
+                expect(timer1).not.to.be.equal(timer2);
+
+                component.setAttribute("another", 'done!');
+
+                setTimeout(() => {
+                    
+                    const counter3 = component.querySelector("#div-count").innerHTML;
+                    const timer3 = component.querySelector("#div-date").innerHTML;
+    
+                    expect(component.props.attributes.count).to.be.equal('2');
+                    expect(component.props.attributes.another).to.be.equal('done!');
+                    expect(counter2).to.be.equal(counter3);
+                    expect(timer2).to.be.equal(timer3);
+    
+                    done();
+    
+                }, 10);
+
+            }, 10);
+
+        }, 10);
+
+    });
+
 
 });
